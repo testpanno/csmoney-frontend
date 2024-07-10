69b6ec0d867583e51d584e4e43fd603d3ca7823f
testpanno <stas.mega.96@bk.ru>
2024-07-11 00:29:33 +0700

    Небольшая локализация пагинации

--- src/components/ui/AuthTable/DataTablePagination.tsx
+++ src/components/ui/AuthTable/DataTablePagination.tsx
@@ -24,12 +24,11 @@ import {
     return (
       <div className="flex items-center justify-between px-2">
         <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
+          
         </div>
-        <div className="flex items-center space-x-6 lg:space-x-8">
+        <div className="flex items-center space-x-6 lg:space-x-8"> 
           <div className="flex items-center space-x-2">
-            <p className="text-sm font-medium">Rows per page</p>
+            <p className="text-sm font-medium">Результатов на странице</p>
             <Select
               value={`${table.getState().pagination.pageSize}`}
               onValueChange={(value) => {
@@ -48,8 +47,8 @@ import {
               </SelectContent>
             </Select>
           </div>
-          <div className="flex w-[100px] items-center justify-center text-sm font-medium">
-            Page {table.getState().pagination.pageIndex + 1} of{" "}
+          <div className="flex w-[150px] items-center justify-center text-sm font-medium">
+            Страница {table.getState().pagination.pageIndex + 1} из {" "}
             {table.getPageCount()}
           </div>
           <div className="flex items-center space-x-2">
